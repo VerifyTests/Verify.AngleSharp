8608842522f19f30a92d4c950b4a31b88a6a6d63
SimonCropp <simon.cropp@gmail.com>
2020-04-27 12:58:32 +1000

    .

--- src/Verify.AngleSharp.Diffing/VerifyAngleSharpDiffing.cs
+++ src/Verify.AngleSharp.Diffing/VerifyAngleSharpDiffing.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,29 @@ using Verify;
 
 public static class VerifyAngleSharpDiffing
 {
+    public static void AngleSharpDiffingSettings(
+        this VerifySettings settings,
+        Action<IDiffingStrategyCollection> options)
+    {
+        Guard.AgainstNull(settings, nameof(settings));
+        settings.Data["AngleSharpDiffing"] = new CompareSettings(options);
+    }
+
+    static bool GetCompareSettings(
+        this VerifySettings settings,
+        [NotNullWhen(true)] out CompareSettings? pagesSettings)
+    {
+        Guard.AgainstNull(settings, nameof(settings));
+        if (settings.Data.TryGetValue("AngleSharpDiffing", out var value))
+        {
+            pagesSettings = (CompareSettings) value;
+            return true;
+        }
+
+        pagesSettings = null;
+        return false;
+    }
+
     public static void Initialize(Action<IDiffingStrategyCollection>? action = null)
     {
         Task<bool> Func(VerifySettings settings, Stream stream1, Stream stream2) =>
@@ -30,10 +54,12 @@ public static class VerifyAngleSharpDiffing
         {
             builder.WithOptions(action);
         }
+
         if (settings.GetCompareSettings(out var innerSettings))
         {
             builder.WithOptions(innerSettings.Action);
         }
+
         var diffs = builder.Build();
         return !diffs.Any();
     }
