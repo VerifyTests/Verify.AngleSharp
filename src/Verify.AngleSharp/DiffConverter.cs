bedd26ca4887fbceaff368570cbfb4e6e3553102
Simon Cropp <simon.cropp@gmail.com>
2023-06-29 10:26:30 +1000

    Revert "Update DiffConverter.cs"
    This reverts commit fea76cad17d52cbd0d6afb8a900dc60e42432031.

--- src/Verify.AngleSharp/DiffConverter.cs
+++ src/Verify.AngleSharp/DiffConverter.cs
@@ -4,75 +4,69 @@ static class DiffConverter
 {
     public static void Append(IDiff diff, StringBuilder builder)
     {
-        switch (diff)
+        if (diff is NodeDiff node)
         {
-            case NodeDiff(var verified, var received, _):
-            {
-                builder.AppendLine($"""
-                                     * Node Diff
-                                       Path: {received.Path}
-                                       Received: {received.Node.NodeValue}
-                                       Verified: {verified.Node.NodeValue}
-                                    """);
-                return;
-            }
-            case AttrDiff(var verified, var received, _):
-            {
-                builder.AppendLine($"""
-                                     * Attribute Diff
-                                       Path: {received.Path}
-                                       Name: {received.Attribute.Name}
-                                       Received: {received.Attribute.Value}
-                                       Verified: {verified.Attribute.Value}
-                                    """);
-                return;
-            }
-            case UnexpectedAttrDiff unexpectedAttribute:
-            {
-                var source = unexpectedAttribute.Test;
-                builder.AppendLine($"""
-                                     * Unexpected Attribute
-                                       Path: {source.Path}
-                                       Name: {source.Attribute.Name}
-                                       Value: {source.Attribute.Value}
-                                    """);
-                return;
-            }
-            case UnexpectedNodeDiff unexpectedNode:
-            {
-                var source = unexpectedNode.Test;
-                builder.AppendLine($"""
-                                     * Unexpected Node
-                                       Path: {source.Path}
-                                       Name: {source.Node.NodeName}
-                                       Value: {source.Node.NodeValue}
-                                    """);
-                return;
-            }
-            case MissingAttrDiff missingAttribute:
-            {
-                var source = missingAttribute.Control;
-                builder.AppendLine($"""
-                                     * Missing Attribute
-                                       Path: {source.Path}
-                                       Name: {source.Attribute.Name}
-                                       Value: {source.Attribute.Value}
-                                    """);
-                return;
-            }
-            case MissingNodeDiff missingNode:
-            {
-                var source = missingNode.Control;
-                builder.AppendLine($"""
-                                     * Missing Node
-                                       Path: {source.Path}
-                                       Name: {source.Node.NodeName}
-                                       Value: {source.Node.NodeValue}
-                                    """);
-                return;
-            }
-            default:
-                throw new($"Unknown diff: {diff.GetType()}");
+            var received = node.Test;
+            var verified = node.Control;
+            builder.AppendLine($@" * Node Diff
+   Path: {received.Path}
+   Received: {received.Node.NodeValue}
+   Verified: {verified.Node.NodeValue}");
+            return;
         }
+
+        if (diff is AttrDiff attribute)
+        {
+            var received = attribute.Test;
+            var verified = attribute.Control;
+            builder.AppendLine($@" * Attribute Diff
+   Path: {received.Path}
+   Name: {received.Attribute.Name}
+   Received: {received.Attribute.Value}
+   Verified: {verified.Attribute.Value}");
+            return;
+        }
+
+        if (diff is UnexpectedAttrDiff unexpectedAttribute)
+        {
+            var source = unexpectedAttribute.Test;
+            builder.AppendLine($@" * Unexpected Attribute
+   Path: {source.Path}
+   Name: {source.Attribute.Name}
+   Value: {source.Attribute.Value}");
+            return;
+        }
+
+        if (diff is UnexpectedNodeDiff unexpectedNode)
+        {
+            var source = unexpectedNode.Test;
+            builder.AppendLine($@" * Unexpected Node
+   Path: {source.Path}
+   Name: {source.Node.NodeName}
+   Value: {source.Node.NodeValue}");
+            return;
+        }
+
+        if (diff is MissingAttrDiff missingAttribute)
+        {
+            var source = missingAttribute.Control;
+            builder.AppendLine($@" * Missing Attribute
+   Path: {source.Path}
+   Name: {source.Attribute.Name}
+   Value: {source.Attribute.Value}");
+            return;
+        }
+
+        if (diff is MissingNodeDiff missingNode)
+        {
+            var source = missingNode.Control;
+            builder.AppendLine($@" * Missing Node
+   Path: {source.Path}
+   Name: {source.Node.NodeName}
+   Value: {source.Node.NodeValue}");
+            return;
+        }
+
+        throw new($"Unknown diff: {diff.GetType()}");
     }
 }
