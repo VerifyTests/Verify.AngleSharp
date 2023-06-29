c5330d5480228dc2c3b6f9f8c2fc5c076c5d2d26
Simon Cropp <simon.cropp@gmail.com>
2023-06-29 10:27:21 +1000

    Update DiffConverter.cs

--- src/Verify.AngleSharp/DiffConverter.cs
+++ src/Verify.AngleSharp/DiffConverter.cs
@@ -8,10 +8,12 @@ static class DiffConverter
         {
             var received = node.Test;
             var verified = node.Control;
-            builder.AppendLine($@" * Node Diff
-   Path: {received.Path}
-   Received: {received.Node.NodeValue}
-   Verified: {verified.Node.NodeValue}");
+            builder.AppendLine($"""
+                                 * Node Diff
+                                   Path: {received.Path}
+                                   Received: {received.Node.NodeValue}
+                                   Verified: {verified.Node.NodeValue}
+                                """);
             return;
         }
 
@@ -19,51 +21,61 @@ static class DiffConverter
         {
             var received = attribute.Test;
             var verified = attribute.Control;
-            builder.AppendLine($@" * Attribute Diff
-   Path: {received.Path}
-   Name: {received.Attribute.Name}
-   Received: {received.Attribute.Value}
-   Verified: {verified.Attribute.Value}");
+            builder.AppendLine($"""
+                                 * Attribute Diff
+                                   Path: {received.Path}
+                                   Name: {received.Attribute.Name}
+                                   Received: {received.Attribute.Value}
+                                   Verified: {verified.Attribute.Value}
+                                """);
             return;
         }
 
         if (diff is UnexpectedAttrDiff unexpectedAttribute)
         {
             var source = unexpectedAttribute.Test;
-            builder.AppendLine($@" * Unexpected Attribute
-   Path: {source.Path}
-   Name: {source.Attribute.Name}
-   Value: {source.Attribute.Value}");
+            builder.AppendLine($"""
+                                 * Unexpected Attribute
+                                   Path: {source.Path}
+                                   Name: {source.Attribute.Name}
+                                   Value: {source.Attribute.Value}
+                                """);
             return;
         }
 
         if (diff is UnexpectedNodeDiff unexpectedNode)
         {
             var source = unexpectedNode.Test;
-            builder.AppendLine($@" * Unexpected Node
-   Path: {source.Path}
-   Name: {source.Node.NodeName}
-   Value: {source.Node.NodeValue}");
+            builder.AppendLine($"""
+                                 * Unexpected Node
+                                   Path: {source.Path}
+                                   Name: {source.Node.NodeName}
+                                   Value: {source.Node.NodeValue}
+                                """);
             return;
         }
 
         if (diff is MissingAttrDiff missingAttribute)
         {
             var source = missingAttribute.Control;
-            builder.AppendLine($@" * Missing Attribute
-   Path: {source.Path}
-   Name: {source.Attribute.Name}
-   Value: {source.Attribute.Value}");
+            builder.AppendLine($"""
+                                 * Missing Attribute
+                                   Path: {source.Path}
+                                   Name: {source.Attribute.Name}
+                                   Value: {source.Attribute.Value}
+                                """);
             return;
         }
 
         if (diff is MissingNodeDiff missingNode)
         {
             var source = missingNode.Control;
-            builder.AppendLine($@" * Missing Node
-   Path: {source.Path}
-   Name: {source.Node.NodeName}
-   Value: {source.Node.NodeValue}");
+            builder.AppendLine($"""
+                                 * Missing Node
+                                   Path: {source.Path}
+                                   Name: {source.Node.NodeName}
+                                   Value: {source.Node.NodeValue}
+                                """);
             return;
         }
 
