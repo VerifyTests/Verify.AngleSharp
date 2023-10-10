77ab244aae4565815850fc2f70b3f1a64763bcaf
Simon Cropp <simon.cropp@gmail.com>
2023-10-11 10:38:46 +1100

    refs

--- src/Verify.AngleSharp/AngleSharpExtensions.cs
+++ src/Verify.AngleSharp/AngleSharpExtensions.cs
@@ -10,7 +10,7 @@ public static class AngleSharpExtensions
         foreach (var node in nodes.ToList())
         {
             yield return node;
-            foreach (var element in node.DescendentsAndSelf())
+            foreach (var element in node.DescendantsAndSelf())
             {
                 yield return element;
             }
@@ -21,22 +21,22 @@ public static class AngleSharpExtensions
         nodes.Descendants().OfType<TNode>();
 
     public static IEnumerable<TNode> Descendants<TNode>(this IEnumerable<INode> elements) =>
-        elements.SelectMany(_ => _.Descendents<TNode>());
+        elements.SelectMany(_ => _.Descendants<TNode>());
 
     public static IEnumerable<INode> Descendants(this IEnumerable<INode> elements) =>
-        elements.SelectMany(_ => _.Descendents());
+        elements.SelectMany(_ => _.Descendants());
 
     public static IEnumerable<TNode> DescendantsAndSelf<TNode>(this IEnumerable<INode> elements) =>
-        elements.SelectMany(_ => _.DescendentsAndSelf<TNode>());
+        elements.SelectMany(_ => _.DescendantsAndSelf<TNode>());
 
     public static IEnumerable<INode> DescendantsAndSelf(this IEnumerable<INode> elements) =>
-        elements.SelectMany(_ => _.DescendentsAndSelf());
+        elements.SelectMany(_ => _.DescendantsAndSelf());
 
     public static IEnumerable<INode> Descendants(this INodeList nodes)
     {
         foreach (var node in nodes.ToList())
         {
-            foreach (var element in node.DescendentsAndSelf())
+            foreach (var element in node.DescendantsAndSelf())
             {
                 yield return element;
             }
