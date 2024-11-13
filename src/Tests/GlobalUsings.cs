b8b4995236d1f0b35426568e94ed79e246e9d1b1
Simon Cropp <simon.cropp@gmail.com>
2024-11-13 21:28:27 +1100

    Add explicit net9 target (#544)
    * Add explicit net9 target
    
    * Update Directory.Build.props
    
    * Docs changes
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/Tests/GlobalUsings.cs
+++ src/Tests/GlobalUsings.cs
@@ -1,2 +1,7 @@
 ﻿global using System.Runtime.CompilerServices;
+global using AngleSharp.Diffing;
+global using AngleSharp.Diffing.Core;
+global using AngleSharp.Dom;
+global using AngleSharp.Html.Parser;
+global using VerifyTests.AngleSharp;
 global using VerifyTests.DiffPlex;
